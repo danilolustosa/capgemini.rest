9df7af401f070c2cacc6a811d0f1a844c8cf6158
Danilo Lustosa <danilolustosa@gmail.com>
2020-11-24 10:21:04 -0300

    second commit

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rest.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+
+
+
+        protected IActionResult TratarResultado(Func<IActionResult> servico)
+        {
+            try
+            {
+                return servico();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro genérico");
+            }            
+        }
+    }
+}
--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -9,25 +10,27 @@ namespace Rest.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CustomerController
+    public class CustomerController : BaseController
     {
 
         [HttpGet]
-        public IEnumerable<Customer> Get()
-        {
+        public IActionResult Get() => TratarResultado(() => {
             List<Customer> lista = new List<Customer>();
 
             lista.Add(new Customer() { Id = 1, Name = "Maria" });
             lista.Add(new Customer() { Id = 2, Name = "Antônio" });
             lista.Add(new Customer() { Id = 3, Name = "João" });
 
-            return lista;
-        }
+            return new ObjectResult(lista) { StatusCode = StatusCodes.Status200OK };
+        });
 
         [HttpPost]
-        public void Set([FromBody] Customer request)
-        {
+        public IActionResult Set([FromBody] Customer request) => TratarResultado(() => {
+
             Console.WriteLine($"Id: {request.Id} ----- Name: {request.Name}");
-        }
+
+            return new ObjectResult(new object()) { StatusCode = StatusCodes.Status201Created };
+
+        });
     }
 }
